bbf7c2229a11ca54fd6006337cd29c3a5aa6da2f
gih011 <137410006+gih011@users.noreply.github.com>
2024-10-17 09:58:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Nome',
+    'Geovana Colonhesi',
+    '2B - N11'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente fica Monaco?',
+    'Fica no continente eurpeu?'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Se diz Good MOrning'
+)
